1b203c04c718de110599d8234b183cf0ae53df55
amahiro <hirokih2938@gmail.com>
2021-07-07 17:29:32 +0900

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+//画面遷移
+$(window).on('load',function() {
+        $('body').addClass('appear');
+});
+//フェードインアニメーション
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
+//ハンバーガーメニュー
+new Hamburger();
